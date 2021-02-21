f54cd5648e7c019bd351e29fe3f193563d458c75
Bilal Shafiq <mrbilalshafiq@gmail.com>
2021-02-21 23:40:26 +0000

    Added nginx feature and account number displays now

--- 1-frontend/app/forms.py
+++ 1-frontend/app/forms.py
@@ -2,4 +2,4 @@ from flask_wtf import FlaskForm
 from wtforms import SubmitField
 
 class IndexForm(FlaskForm):
-    submit = SubmitField('Get Account Number')
+    submit = SubmitField('Get Prize!')
